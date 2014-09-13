470c56cb30b6eb9144638a16bf6f9a3f91a5d1a7
David Cao <davidcao@seas1064.wireless-pennnet.upenn.edu>
2014-09-13 14:15:20 -0400

    Initial Commit

---
+++ WakeUsUp/WUUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WUUAppDelegate.h
+//  WakeUsUp
+//
+//  Created by David Cao on 9/13/14.
+//  Copyright (c) 2014 David.Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WUUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WakeUsUp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WakeUsUp
+//
+//  Created by David Cao on 9/13/14.
+//  Copyright (c) 2014 David.Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WUUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WUUAppDelegate class]));
+    }
+}
